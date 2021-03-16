6dade67c26db41fe9f2c17c242ef559e8aab6d5f
Hussein-Mahfouz <husseinmahfouz93@gmail.com>
2021-03-16 21:19:07 +0200

    appendix

--- R/build.R
+++ R/build.R
@@ -1,12 +1,12 @@
 # Aim: reproduce results for other UK cities
 
-chosen_city <- "Leeds"
+chosen_city <- "Birmingham"
 
 source("R/_1.0_get_flow_data.R")
 
-# Get open data needed for 2nd script. Warning: 100 MB file
-piggyback::pb_download("MSOA_2011_Boundaries.zip", tag = "1")
-unzip("MSOA_2011_Boundaries.zip", exdir = "data-raw")
+# # Get open data needed for 2nd script. Warning: 100 MB file  (HM : Uncomment Later)
+# piggyback::pb_download("MSOA_2011_Boundaries.zip", tag = "1")
+# unzip("MSOA_2011_Boundaries.zip", exdir = "data-raw")
 
 # file.edit("R/_2.0_distance_and_elevation.R")
 source("R/_2.0_distance_and_elevation.R")
@@ -14,10 +14,7 @@ source("R/_2.0_distance_and_elevation.R")
 # file.edit("R/_3.0_potential_demand.R")
 source("R/_3.0_potential_demand.R")
 # check if figures have been created
-magick::image_read("data/Leeds/Plots/mode_share_increase_vs_performance_smooth_Leeds.png")
 
-remotes::install_cran("waffle")
-remotes::install_cran("latex2exp")
 
 source("R/_3.1_plot_mode_shares.R")
 source("R/_3.2_plot_od_comparison.R")
@@ -26,9 +23,9 @@ source("R/_3.3_plot_desire_lines_current_vs_potential.R")
 
 source("R/_4.0_aggregating_flows.R")
 
-file.exists("data/Leeds/msoa_lon_lat.shp")
-file.exists("data/Leeds/graph_with_flows_unweighted.Rds")
-# file.edit("R/_5.0_identifying_cycle_infastructure_from_osm_tags.R")
+# file.exists("data/Leeds/msoa_lon_lat.shp")
+# file.exists("data/Leeds/graph_with_flows_unweighted.Rds")
+
 # Warning: takes several minutes to run
 source("R/_5.0_identifying_cycle_infastructure_from_osm_tags.R")
 
