03e537bfbcd3c1598d4adb436225812a14921b23
Hussein Mahfouz <45176416+Hussein-Mahfouz@users.noreply.github.com>
2023-09-22 18:06:06 +0100

    flowchart

--- latexdiff.R
+++ latexdiff.R
@@ -1,12 +1,27 @@
 # Aim: generate diff for peer review
 
+# ROUND 1: Robin
 
+# download.file(
+#   "https://github.com/Hussein-Mahfouz/cycle-networks/raw/ce4827fc708d510bead10d4998ce7ba875286d0d/README.Rmd",
+#   "paper-original-submission.Rmd"
+#   )
+# file.edit("paper-original-submission.Rmd")
+# rmarkdown::render("paper-original-submission.Rmd")
+# system("latexdiff paper-original-submission.tex README.tex > tracked-changes.tex")
+# tinytex::pdflatex("tracked-changes.tex")
+# browseURL("tracked-changes.pdf")
+
+# ROUND 2: Hussein
+
+# download paper version submitted after round 1 of reviews -> save it as "paper_round_1"
 download.file(
-  "https://github.com/Hussein-Mahfouz/cycle-networks/raw/ce4827fc708d510bead10d4998ce7ba875286d0d/README.Rmd",
-  "paper-original-submission.Rmd"
-  )
-file.edit("paper-original-submission.Rmd")
-rmarkdown::render("paper-original-submission.Rmd")
-system("latexdiff paper-original-submission.tex README.tex > tracked-changes.tex")
-tinytex::pdflatex("tracked-changes.tex")
-browseURL("tracked-changes.pdf")
+  "https://github.com/Hussein-Mahfouz/cycle-networks/blob/c3b74611e92b4092af487c1bc179187c9e1608bb/README.Rmd",
+  "paper_round_1.Rmd"
+)
+file.edit("paper_round_1.Rmd")
+rmarkdown::render("paper_round_1.Rmd")
+# compare with current version and create tracked changes
+system("latexdiff paper_round_1.tex README.tex > tracked-changes_2.tex")
+tinytex::pdflatex("tracked-changes_2.tex")
+browseURL("tracked-changes_2.pdf")
